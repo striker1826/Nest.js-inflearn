2b53bb7975ce611cebb2eea0d4ebec2d87f452c2
LAPTOP-PQPCKK5H\alstj <alstjq123579@daum.net>
2022-12-22 03:45:18 +0900

    첫 커밋

---
+++ src/app.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Req } from '@nestjs/common';
+import { Body, Param } from '@nestjs/common/decorators';
+import { AppService } from './app.service';
+import { CatsService } from './cats/cats.service';
+
+@Controller('cats')
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private readonly catsService: CatsService,
+  ) {}
+
+  @Get('hello/:id/:name')
+  getHello(
+    @Req() req: string,
+    @Body() body,
+    @Param() param: { id: string; name: string },
+  ): string {
+    console.log(param);
+    return this.appService.getHello();
+  }
+}
---
+++ src/cats/cats.schema.ts
@@ -0,0 +1,40 @@
+/* eslint-disable prettier/prettier */
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, SchemaOptions } from 'mongoose';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+const options: SchemaOptions = {
+  timestamps: true,
+};
+
+@Schema(options)
+export class Cat extends Document {
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  @IsString()
+  email: string;
+
+  @Prop({
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @Prop({
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @Prop()
+  @IsString()
+  imgUrl: string;
+}
+
+export const Catschema = SchemaFactory.createForClass(Cat);
